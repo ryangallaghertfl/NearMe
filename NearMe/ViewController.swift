ff755dc32e75d1b80aa5e81dbb34dba9399db93e
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-27 16:41:30 +0000

    Initial Commit

---
+++ NearMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Ryan Gallagher on 27/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
