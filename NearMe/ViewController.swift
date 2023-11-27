ff75bbec633876244c295a9a405bcd7a0c93d36b
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-27 17:27:09 +0000

    VC: added setup mapview instance inside VC, set layout constraints

--- NearMe/ViewController.swift
+++ NearMe/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        map.showsUserLocation = true
+        return map
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.red
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+        view.addSubview(mapView)
+        
+        //add constraints to mapView
+        
+        mapView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        mapView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
 
