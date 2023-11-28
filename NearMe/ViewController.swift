27cdbd69446a11ac0d0f061b158f77ef01a97a58
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-28 08:27:24 +0000

    VC: added UITextField with properties

--- NearMe/ViewController.swift
+++ NearMe/ViewController.swift
@@ -16,6 +16,18 @@ class ViewController: UIViewController {
         map.showsUserLocation = true
         return map
     }()
+    
+    lazy var searchTextField: UITextField = {
+        let searchTextField = UITextField()
+        searchTextField.layer.cornerRadius = 10
+        searchTextField.clipsToBounds = true
+        searchTextField.backgroundColor = UIColor.white
+        searchTextField.placeholder = "Search"
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0)) //leftview manages the appearance of a view on the left side of text field
+        searchTextField.leftViewMode = .always
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
