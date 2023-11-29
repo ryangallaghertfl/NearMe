0a2e93b833f5656c2522c784f9c21e4b8838d30a
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-29 14:47:15 +0000

    PlacesTableVC: added outline of class to display results

---
+++ NearMe/Controllers/PlacesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlacesTableViewController.swift
+//  NearMe
+//
+//  Created by Ryan Gallagher on 29/11/2023.
+//
+
+import Foundation
+import UIKit
+
+class PlacesTableViewController: UITableViewController {
+    
+}
