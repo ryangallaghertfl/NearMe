169302c474b7d069adb1ed792cb802131ca15270
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-29 17:17:20 +0000

    PlacesDetailVC: implemented with place PlaceAnnotation property

---
+++ NearMe/Controllers/PlaceDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PlaceDetailViewController.swift
+//  NearMe
+//
+//  Created by Ryan Gallagher on 29/11/2023.
+//
+
+import Foundation
+import UIKit
+
+class PlaceDetailViewController: UIViewController {
+    
+    let place: PlaceAnnotation
+    
+    init(place: PlaceAnnotation) {
+        self.place = place
+        super.init(nibName: nil, bundle: nil)
+        setupUI()
+    }
+    
+    
+    
+    //MARK: setting up UI
+    
+    private func setupUI() {
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
