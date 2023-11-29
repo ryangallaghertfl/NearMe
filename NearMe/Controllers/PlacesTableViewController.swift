075354821b94c8ec3875e2e6a7dfa4a9a49f65dc
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-29 15:01:48 +0000

    PlacesTableVC: added properties to handle places and userLocation, called super.init without bundles

--- NearMe/Controllers/PlacesTableViewController.swift
+++ NearMe/Controllers/PlacesTableViewController.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class PlacesTableViewController: UITableViewController {
+    var userLocation: CLLocation
+    let places: ([PlaceAnnotation])
     
+    init(userLocation: CLLocation, places: [PlaceAnnotation]) {
+        self.userLocation = userLocation
+        self.places = places
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
