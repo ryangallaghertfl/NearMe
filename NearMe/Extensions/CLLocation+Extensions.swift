c96f6400178805b60e523a2ea9c13a7d9ba9bb9d
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-11-29 12:23:44 +0000

    Model: added default location extension for CLLocation

---
+++ NearMe/Extensions/CLLocation+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  CLLocation+Extensions.swift
+//  NearMe
+//
+//  Created by Ryan Gallagher on 29/11/2023.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocation {
+    
+    static var `default`: CLLocation {
+        CLLocation(latitude: 51.4779, longitude: 0.0015)
+    }
+}
+
